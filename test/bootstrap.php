f37e0c62b2f26742f55e7ed98e699e9e812078db
Dániel Buga <bugadani@gmail.com>
2015-08-05 21:29:10 +0200

    Update phpunit.xml and license files

--- test/bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-if (is_file('vendor/autoload.php')) {
-    require_once 'vendor/autoload.php';
-} elseif (is_file('../vendor/autoload.php')) {
-    require_once '../vendor/autoload.php';
-}
